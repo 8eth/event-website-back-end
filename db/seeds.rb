502bb8baa1b50dd45e312cc5453b8d17eecf88fb
Betelihem <Betelihem>
2022-03-29 15:24:30 -0700

    added pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,17 @@ puts "🌱 Seeding guests..."
 end
 
 puts "🌱 Seeding pictures..."
-6.times do
-    Picture.create(caption: "picture of #{Faker::FunnyName.name}", img_link: "https://i.picsum.photos/id/782/200/300.jpg?hmac=VWiFPQfyEC9R-Cj6iOqYU6jCf23goz4cBkBc28drqE0", party_id: p1.id)
-end
+# 5.times do
+#     Picture.create(caption: "#{Faker::FunnyName.name}", img_link: "https://i.picsum.photos/id/782/200/300.jpg?hmac=VWiFPQfyEC9R-Cj6iOqYU6jCf23goz4cBkBc28drqE0", party_id: p1.id)
+# end
+
+Picture.create(caption: "cake", img_link: "../pictures/cake.jpg", party_id: p1.id)
+Picture.create(caption: "ceremony", img_link: "../pictures/ceremony.jpg", party_id: p1.id)
+Picture.create(caption: "dinner", img_link: "../pictures/dinner.jpg", party_id: p1.id)
+Picture.create(caption: "dinner2", img_link: "../pictures/dinner2.jpg", party_id: p1.id)
+Picture.create(caption: "flowers", img_link: "../pictures/flowers.jpg", party_id: p1.id)
+
+
 
 
 puts "✅ Done seeding!"
